fc8c15ae0dde68796b424e7234c4dba4a3194cc1
saurabhlm10 <saurabhlm10@gmail.com>
2024-04-28 03:31:15 +0530

    feat: added captcha

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+// This uses the rate limited backend
+import { Turnstile } from "@marsidev/react-turnstile";
+
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [token, setToken] = useState<string>("");
+
+  return (
+    <>
+      <input placeholder="OTP"></input>
+      <input placeholder="New password"></input>
+
+      <Turnstile
+        onSuccess={(token) => {
+          setToken(token);
+        }}
+        siteKey="0x4AAAAAAAYfY64vFenESIRV"
+      />
+
+      <button
+        onClick={() => {
+          // This is the ratelimiting backend
+          axios.post("http://localhost:3000/reset-password", {
+            email: "harkirat@gmail.com",
+            otp: "123456",
+            token: token,
+          });
+        }}
+      >
+        Update password
+      </button>
+    </>
+  );
+}
+
+export default App;
